321e4f6b19f08af3edd5ee3a1beb601fedd0f6a1
annProg <tecbbs@qq.com>
2021-12-07 00:15:56 +0800

    first commit

---
+++ module.itopcn-cn-optimize.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2020 ops-itop
+ * @license     NIT
+ */
+
+SetupWebPage::AddModule(
+	__FILE__,
+	'itopcn-cn-optimize/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'iTop中国用户优化',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-config-mgmt/2.7.0',
+		),
+		'mandatory' => false,
+		'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
+		'installer' => 'CNOptimizeInstaller',
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.itopcn-cn-optimize.php',
+		),
+		'data.struct' => array(
+		),
+		'data.sample' => array(
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '',
+		'doc.more_information' => '',
+
+		// Default settings
+		//
+		'settings' => array(
+		),
+	)
+);
+
+if (!class_exists('CNOptimizeInstaller')) {
+	class CNOptimizeInstaller extends ModuleInstallerAPI {
+		public static function BeforeWritingConfig(Config $oConfiguration) {
+			$sUrl = $oConfiguration->Get("app_root_url");
+			$oConfiguration->Set("app_icon_url", $sUrl, "yes");
+			$oConfiguration->Set("timezone", "Asia/Shanghai");
+			$oConfiguration->Set("export_pdf_font", "DroidSansFallback", "yes");
+			$oConfiguration->Set("csv_file_default_charset", "UTF-8");
+			return $oConfiguration;
+		}
+	}
+}
---
+++ zh_cn.dict.itopcn-cn-optimize.php
@@ -0,0 +1,12 @@
+<?php
+
+Dict::Add('ZH CN', 'Chinese', '简体中文', array(
+	'Class:Person/Name' => '%2$s %1$s',
+
+	'Class:Typology' => '类型',
+	'Class:Typology/Attribute:finalclass' => '类型',
+	'Menu:Typology' => '类型配置',
+	'Menu:Typology+' => '类型配置',
+	'Menu:ConfigManagement:Typology' => '类型配置',
+));
+
